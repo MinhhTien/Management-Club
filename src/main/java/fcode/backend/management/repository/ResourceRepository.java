c1b2260a72d399fe73989b42f29e127cc3028f20
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2022-09-02 09:15:05 +0700

    Complete resource and subject repository

--- src/main/java/fcode/backend/management/repository/ResourceRepository.java
+++ src/main/java/fcode/backend/management/repository/ResourceRepository.java
@@ -1,4 +1,38 @@
 package fcode.backend.management.repository;
 
-public class ResourceRepository {
+import fcode.backend.management.repository.entity.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public interface ResourceRepository extends JpaRepository<Resource, Integer> {
+    Resource findOneById(Integer resourceId);
+    Resource findResourceByUrl(String url);
+    List<Resource> findResourcesBySubjectOrderBySubjectAsc(String subjectId);
+    @Query(nativeQuery = true, value = "SELECT subject_id FROM resource WHERE id = ?1")
+    Integer findSubjectId(Integer resourceId);
+
+    boolean existsByUrl(String resourceUrl);
+    boolean existsById(Integer id);
+
+    @Modifying
+    @Transactional
+    @Query(nativeQuery = true, value = "INSERT INTO resource(url, contributor, description, subject_id) VALUES (?1, ?2, ?3, ?4)")
+    void insertResource(String url, String contributor, String description, Integer subjectId);
+
+    @Modifying
+    @Transactional
+    @Query(nativeQuery = true, value = "UPDATE resource SET url = ?2 WHERE id = ?1")
+    void updateResource(Integer id, String newUrl);
+
+
+    @Modifying
+    @Transactional
+    @Query(nativeQuery = true, value = "DELETE FROM resource WHERE id = ?1")
+    void deleteById(Integer id);
 }
--- src/main/java/fcode/backend/management/repository/SubjectRepository.java
+++ src/main/java/fcode/backend/management/repository/SubjectRepository.java
@@ -1,4 +1,24 @@
 package fcode.backend.management.repository;
 
-public class SubjectRepository {
+import fcode.backend.management.repository.entity.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+
+    Subject findOneById(Integer id);
+
+    boolean existsByName(String name);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM subject")
+    Set<Subject> getAllSubject();
+
+    @Query(nativeQuery = true, value = "SELECT * FROM subject WHERE name = ?1")
+    Subject findByName(String subjectName);
+
+    Set<Subject> findBySemester(Integer semester);
 }
