2bff4bd01445d34cda2bee6653179ee70b635236
duybinhngo <duybinhngo2901@gmail.com>
2022-09-03 16:44:35 +0700

    Create test for question and comment service.

---
+++ src/test/java/fcode/backend/management/CommentServiceTests.java
@@ -0,0 +1,40 @@
+package fcode.backend.management;
+
+import fcode.backend.management.model.dto.CommentDTO;
+import fcode.backend.management.model.dto.QuestionDTO;
+import fcode.backend.management.sevice.CommentService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentServiceTests {
+    @Autowired
+    CommentService commentService;
+    @Test
+    void addComment() {
+        commentService.createComment(new CommentDTO("content comment 1", "authorcomment1@gmail.com", 1));
+        commentService.createComment(new CommentDTO("content comment 2", "authorcomment1@gmail.com", 1));
+        commentService.createComment(new CommentDTO("content comment 3", "author1@gmail.com", 2));
+        commentService.createComment(new CommentDTO("content comment 4", "author2@gmail.com", 3));
+    }
+
+    @Test
+    void getComment() {
+        System.out.println(commentService.getCommentById(1).getData());
+        commentService.getAllCommentsOfAQuestion(new QuestionDTO(1)).getData().forEach(System.out::println);
+    }
+
+    @Test
+    void updateComment() {
+        commentService.updateContent(new CommentDTO(2), "new comment content");
+        commentService.updateContent(new CommentDTO(1), "new comment content 2");
+        commentService.updateContent(new CommentDTO(1), null);
+        commentService.updateContent(new CommentDTO(3), "new content 3");
+    }
+    @Test
+    void deleteComment() {
+        commentService.deleteComment(new CommentDTO(1));
+        commentService.deleteAllCommentByAuthorEmail("authorcomment1@gmail.com");
+    }
+}
---
+++ src/test/java/fcode/backend/management/QuestionServiceTests.java
@@ -0,0 +1,85 @@
+package fcode.backend.management;
+
+import fcode.backend.management.model.dto.QuestionDTO;
+import fcode.backend.management.sevice.QuestionService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.SecondaryTable;
+import java.util.Set;
+
+@SpringBootTest
+class QuestionServiceTests {
+    @Autowired
+    QuestionService questionService;
+    @Test
+    void addQuestion() {
+        questionService.createQuestion(new QuestionDTO("title1", "content1", "author1@gmail.com"));
+        questionService.createQuestion(new QuestionDTO("title2", "content2", "author2@gmail.com"));
+        questionService.createQuestion(new QuestionDTO("title3", "content3", "author1@gmail.com"));
+        questionService.createQuestion(new QuestionDTO("title4", "content4", "author1@gmail.com"));
+
+    }
+
+    @Test
+    void approveQuestion() {
+        questionService.approveQuestion(new QuestionDTO(1));
+        questionService.approveQuestion(new QuestionDTO(5));
+        questionService.disapproveQuestion(new QuestionDTO(4));
+        questionService.approveQuestion(new QuestionDTO(3));
+    }
+
+
+    @Test
+    void getQuestions() {
+        Set<QuestionDTO> questionDTOSet;
+        QuestionDTO questionDTO;
+        questionDTOSet = questionService.getAllQuestions().getData();
+        questionDTOSet.forEach(System.out::println);
+        questionDTO = questionService.getQuestionById(7).getData();
+        System.out.println(questionDTO);
+        questionDTO = questionService.getQuestionById(2).getData();
+        System.out.println(questionDTO);
+        questionDTO = questionService.getQuestionById(4).getData();
+        System.out.println(questionDTO);
+        questionDTO = questionService.getQuestionById(1).getData();
+        System.out.println(questionDTO);
+        questionDTOSet = questionService.getQuestionByAuthor("author3@gmail.com").getData();
+        if (questionDTOSet != null) questionDTOSet.forEach(System.out::println);
+        questionDTOSet = questionService.getQuestionByAuthor("author1@gmail.com").getData();
+        if (questionDTOSet != null) questionDTOSet.forEach(System.out::println);
+        questionDTOSet = questionService.getQuestionByAuthor("author2@gmail.com").getData();
+        if (questionDTOSet != null) questionDTOSet.forEach(System.out::println);
+    }
+
+    @Test
+    void updateQuestion1() {
+        questionService.updateQuestion(new QuestionDTO(3), null, "new content 3");
+
+        questionService.updateQuestion(new QuestionDTO(2), null, "new content 2");
+        questionService.updateQuestion(new QuestionDTO(2), "new title 2", null);
+        questionService.updateTitle(new QuestionDTO(1), null);
+        questionService.updateContent(new QuestionDTO(1), null);
+
+    }
+
+    @Test
+    void updateQuestion2() {
+        questionService.updateQuestion(new QuestionDTO(3), "new title 3", null);
+        questionService.updateTitle(new QuestionDTO(1), "new title 1");
+        questionService.updateContent(new QuestionDTO(1), "new content 1");
+    }
+
+    @Test
+    void deleteQuestion() {
+        questionService.deleteQuestion(new QuestionDTO(2));
+        questionService.deleteQuestionByAuthorEmail("author1@gmail.com");
+    }
+    @Test
+    void restoreQuestion() {
+        questionService.restoreQuestionByAuthorEmail("author1@gmail.com");
+    }
+}
