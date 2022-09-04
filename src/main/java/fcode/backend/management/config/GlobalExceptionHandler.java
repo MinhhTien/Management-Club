e03bd20bb37e1af2c04f3a4358d86eaa531fbb8e
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2022-09-04 07:00:48 +0700

    Fix sevice to service in GlobalExceptionHandler

--- src/main/java/fcode/backend/management/config/GlobalExceptionHandler.java
+++ src/main/java/fcode/backend/management/config/GlobalExceptionHandler.java
@@ -2,7 +2,7 @@ package fcode.backend.management.config;
 
 
 import fcode.backend.management.model.response.Response;
-import fcode.backend.management.sevice.exception.ServiceException;
+import fcode.backend.management.service.exception.ServiceException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
