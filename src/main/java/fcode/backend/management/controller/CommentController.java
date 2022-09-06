ab1bd5c99ed4c54edf770d5be5d1b41bd8bf2ecd
duybinhngo <duybinhngo2901@gmail.com>
2022-09-06 20:15:56 +0700

    Add get all comments of a question into Comment controller.

--- src/main/java/fcode/backend/management/controller/CommentController.java
+++ src/main/java/fcode/backend/management/controller/CommentController.java
@@ -6,6 +6,8 @@ import fcode.backend.management.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Set;
+
 @RequestMapping("/comment")
 public class CommentController {
     @Autowired
@@ -20,7 +22,10 @@ public class CommentController {
     public Response<CommentDTO> getCommentById(@PathVariable Integer commentId) {
         return commentService.getCommentById(commentId);
     }
-
+    @GetMapping("/getByQuestion/{questionId}")
+    public Response <Set<CommentDTO>> getAllCommentsOfAQuestion(@PathVariable Integer questionId) {
+        return commentService.getAllCommentsOfAQuestion(questionId);
+    }
     @PutMapping
     public Response<Void> updateComment(@RequestBody CommentDTO commentDTO ,@RequestAttribute String content) {
         return commentService.updateContent(commentDTO, content);
