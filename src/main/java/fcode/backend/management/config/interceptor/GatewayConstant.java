22d7a9d8c6722553fa0afeb5359246b17b8e688f
Phuoc Huynh <huynhphuoc2468@gmail.com>
2022-09-07 11:17:13 +0700

    Reduce redundant code

--- src/main/java/fcode/backend/management/config/interceptor/GatewayConstant.java
+++ src/main/java/fcode/backend/management/config/interceptor/GatewayConstant.java
@@ -20,12 +20,12 @@ public class GatewayConstant {
     public static void addApiEntities() {
         apiEntities.add(new ApiEntity("Auth api","/auth/**",GET_METHOD, null));
 
-<<<<<<< HEAD
+
         apiEntities.add(new ApiEntity("Challenge Get api", "/challenge/**", GET_METHOD, null));
         apiEntities.add(new ApiEntity("Challenge Post api", "/challenge/**", POST_METHOD, Role.MANAGER));
         apiEntities.add(new ApiEntity("Challenge Put api", "/challenge/**", PUT_METHOD, Role.MANAGER));
         apiEntities.add(new ApiEntity("Challenge Delete api", "/challenge/**", DELETE_METHOD, Role.MANAGER));
-=======
+
         apiEntities.add(new ApiEntity("Subject Get api", "/subject/**",GET_METHOD,null));
         apiEntities.add(new ApiEntity("Subject Post api", "/subject",POST_METHOD, Role.MANAGER));
         apiEntities.add(new ApiEntity("Subject Put api", "/subject",PUT_METHOD,Role.MANAGER));
@@ -35,6 +35,6 @@ public class GatewayConstant {
         apiEntities.add(new ApiEntity("Resource Post api", "/resource",POST_METHOD, Role.MANAGER));
         apiEntities.add(new ApiEntity("Resource Put api", "/resource",PUT_METHOD,Role.MANAGER));
         apiEntities.add(new ApiEntity("Resource Delete api", "/resource/**",DELETE_METHOD,Role.MANAGER));
->>>>>>> e87e3499daa25c214ebb7ec111da45884c8a21a2
+
     }
 }
