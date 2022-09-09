faf2e7b0a08f676e4d95f6adadcd87d345775837
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2022-09-09 22:25:00 +0700

    Create DTO for announcement and notification

--- src/main/java/fcode/backend/management/model/dto/AnnouncementDTO.java
+++ src/main/java/fcode/backend/management/model/dto/AnnouncementDTO.java
@@ -1,4 +1,40 @@
 package fcode.backend.management.model.dto;
 
+import io.swagger.models.auth.In;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class AnnouncementDTO {
+    private Integer id;
+    private String title;
+    private String description;
+    private String infoGroup;
+    private String infoUserId;
+    private String location;
+    private String image_url;
+    private Integer sendEmailWhenUpdate;
+    private String mail;
+    private String mailTitle;
+    private Integer memberId;
+
+    public AnnouncementDTO(String title, String description, String infoGroup, String infoUserId, String location, String image_url, Integer sendEmailWhenUpdate, String mail, String mailTitle, Integer memberId) {
+        this.title = title;
+        this.description = description;
+        this.infoGroup = infoGroup;
+        this.infoUserId = infoUserId;
+        this.location = location;
+        this.image_url = image_url;
+        this.sendEmailWhenUpdate = sendEmailWhenUpdate;
+        this.mail = mail;
+        this.mailTitle = mailTitle;
+        this.memberId = memberId;
+    }
+
+    public AnnouncementDTO(Integer id) {
+        this.id = id;
+    }
 }
--- src/main/java/fcode/backend/management/model/dto/NotificationDTO.java
+++ src/main/java/fcode/backend/management/model/dto/NotificationDTO.java
@@ -1,4 +1,25 @@
 package fcode.backend.management.model.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class NotificationDTO {
+    private Integer id;
+    private String title;
+    private String description;
+    private Integer announcementId;
+
+    public NotificationDTO(Integer id) {
+        this.id = id;
+    }
+
+    public NotificationDTO(String title, String description, Integer announcementId) {
+        this.title = title;
+        this.description = description;
+        this.announcementId = announcementId;
+    }
 }
