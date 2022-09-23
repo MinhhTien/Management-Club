80df04eccde1bcd571a455d6a260dcfd2a9bed85
duybinhngo <duybinhngo2901@gmail.com>
2022-09-23 17:18:27 +0700

    Add crew controller.

---
+++ src/main/java/fcode/backend/management/controller/CrewAnnouncementController.java
@@ -0,0 +1,70 @@
+package fcode.backend.management.controller;
+
+import fcode.backend.management.model.dto.CrewAnnouncementDTO;
+import fcode.backend.management.model.response.Response;
+import fcode.backend.management.service.CrewAnnouncementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/crewAnnouncement")
+public class CrewAnnouncementController {
+    @Autowired
+    CrewAnnouncementService crewAnnouncementService;
+    @PostMapping
+    public Response<Void> createCrewAnnouncement(@RequestBody CrewAnnouncementDTO crewAnnouncementDTO) {
+        return crewAnnouncementService.createCrewAnnouncement(crewAnnouncementDTO);
+    }
+
+    @GetMapping("/{crewAnnouncementId}")
+    public Response<CrewAnnouncementDTO> getCrewAnnouncementById(@PathVariable Integer crewAnnouncementId) {
+        return crewAnnouncementService.getCrewAnnouncementById(crewAnnouncementId);
+    }
+
+    @GetMapping("/all")
+    public Response<Set<CrewAnnouncementDTO>> getAllCrewAnnouncement() {
+        return crewAnnouncementService.getAllCrewAnnouncements();
+    }
+
+    @GetMapping("/processing")
+    public Response<Set<CrewAnnouncementDTO>> getProcessingCrewAnnouncement() {
+        return crewAnnouncementService.getProcessingCrewAnnouncements();
+    }
+    @GetMapping("/inactive")
+    public Response<Set<CrewAnnouncementDTO>> getInactiveCrewAnnouncement() {
+        return crewAnnouncementService.getInactiveCrewAnnouncements();
+    }
+
+    @PutMapping("/approve/{crewAnnouncementId}")
+    public Response<Void> approveAnnouncement(@PathVariable Integer crewAnnouncementId) {
+        return crewAnnouncementService.approveCrewAnnouncement(crewAnnouncementId);
+    }
+
+    @PutMapping("/disapprove/{crewAnnouncementId}")
+    public Response<Void> disapproveAnnouncement(@PathVariable Integer crewAnnouncementId) {
+        return crewAnnouncementService.disapproveCrewAnnouncement(crewAnnouncementId);
+    }
+
+    @PutMapping("/approve/all")
+    public Response<Void> approveAll() {
+        return crewAnnouncementService.approveAll();
+    }
+
+    @PutMapping("/disapprove/all")
+    public Response<Void> disapproveAll() {
+        return crewAnnouncementService.disapproveAll();
+    }
+
+    @PutMapping
+    public Response<Void> updateCrewAnnouncement(@RequestBody CrewAnnouncementDTO crewAnnouncementDTO) {
+        return crewAnnouncementService.updateCrewAnnouncement(crewAnnouncementDTO);
+    }
+
+    @DeleteMapping("/{crewAnnouncementId}")
+    public Response<Void> deleteCrewAnnouncement(@PathVariable Integer crewAnnouncementId) {
+        return crewAnnouncementService.deleteCrewAnnouncementById(crewAnnouncementId);
+    }
+
+}
