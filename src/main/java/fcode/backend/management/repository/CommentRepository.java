c07d38152b08c5ca7db65404c86b39ca0fb99719
duybinhngo <duybinhngo2901@gmail.com>
2022-12-09 16:39:41 +0700

    Change from set to list.

--- src/main/java/fcode/backend/management/repository/CommentRepository.java
+++ src/main/java/fcode/backend/management/repository/CommentRepository.java
@@ -6,11 +6,12 @@ import fcode.backend.management.service.constant.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Set;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
     Comment findCommentByIdAndStatus(Integer id, Status status);
     Comment findCommentByIdAndStatusIsNot(Integer id, Status status);
     Set<Comment> findCommentByAuthorEmailAndStatusIsNot(String authorEmail, Status status);
-    Set<Comment> findTop10ByQuestionAndStatusOrderByCreatedTimeDesc(Question question, Status status);
+    List<Comment> findTop10ByQuestionAndStatusOrderByCreatedTimeDesc(Question question, Status status);
 }
